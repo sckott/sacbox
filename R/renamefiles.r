bdafb93a6e32e95f617cc3ddc6a0fd148aa7b2c0
Scott Chamberlain <myrmecocystus@gmail.com>
2012-07-02 14:35:15 -0700

    added renamefiles fxn, which renames files in a directory using regex

---
+++ R/renamefiles.r
@@ -0,0 +1,29 @@
+#' Rename a bunch of files in a directory with the same regex procedure.
+#' 
+#' For example, a file name may have spaces, and you can replace them with "_".
+#' 
+#' @import plyr stringr
+#' @param dir Path to directory for which you want to change the file names.
+#' @param pattern Pattern in file name to replace.
+#' @param replacement Text to replace pattern with.
+#' @return Prints "Operation complete" to console when done; look at files 
+#'    in directory 'dir' for changes.
+#' @author Scott Chamberlain \email{myrmecocystus@@gmail.com}
+#' @keywords file rename
+#' @examples \dontrun{
+#' renamefiles("~/test", " ", "_")
+#' }
+#' @export
+renamefiles <- function(dir, pattern, replacement){
+  
+  setwd(dir)
+  
+  rename_ <- function(x) {
+    file.rename(x, str_replace_all(x, pattern, replacement))
+  }
+  
+  files <- dir(full.names=T)
+  l_ply(files, rename_)
+  message("Operation complete") 
+  
+}
