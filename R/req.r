cc58b57418a10add760dbffc0e79b8c0a0826618
Scott Chamberlain <myrmecocystus@gmail.com>
2012-07-06 14:44:31 -0700

    added req fxn to namespace

--- R/req.r
+++ R/req.r
@@ -3,6 +3,7 @@
 #' @param x Vector of quoted package names to load. 
 #' @examples
 #' req(list("XML", "doMC", "plyr", "RCurl", "stringr"))
+#' @export
 req <- function(x) {
   lapply(x, FUN = function(X) {
     do.call("require", list(X)) 
