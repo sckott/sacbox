ecb689808d5c74ad22090ce1bb194d8db04d4a8f
Scott Chamberlain <myrmecocystus@gmail.com>
2012-09-11 15:58:57 -0700

    simbal updated

--- R/simbal.r
+++ R/simbal.r
@@ -16,72 +16,117 @@
 #' # Simulate 20 trees, each with 10 tips (=species), then pull out trees and metrics, 
 #' # using the colless' statistic (fxn: colless) of the apTreeshape package
 #' out <- simbal(t = 10, metric = "colless", n = 20, cutlow = -0.5, cuthigh = 0.5) # run it
-#' as.numeric(sapply(out, "[", "colstat")) # get the balance metric values
-#' compact(lapply(out, function(x) x$colless_bal)) # get the balanced trees
-#' compact(lapply(out, function(x) x$colless_unbal)) # get the unbalanced trees
+#' out$bal # get the balanced trees
+#' out$unbal # get the unbalanced trees
 #' 
 #' # Using beta-splitting (fxn: maxlik.betasplit) metric of the apTreeshape package
-#' out <- simbal(t = 10, metric = "beta", n = 20, cutlow = -0.5, cuthigh = 0.5) # run it
-#' as.numeric(sapply(out, "[", "beta")) # get the balance metric values
-#' compact(lapply(out, function(x) x$beta_bal)) # get the balanced trees
-#' compact(lapply(out, function(x) x$beta_unbal)) # get the unbalanced trees
+#' out <- simbal(t = 10, metric = "beta", n = 100, cutlow = -0.5, cuthigh = 0.5) # run it
+#' out$bal # get the balanced trees
+#' out$unbal # get the unbalanced trees
 #' 
 #' # Using gamma statistic (fxn: gammaStat) metric of the ape package
-#' out <- simbal(t = 10, metric = "gamma", n = 1000, cutlow = 1, cuthigh = 3) # run it
-#' as.numeric(sapply(out, "[", "gamstat")) # get the balance metric values
-#' trees <- compact(lapply(out, function(x) x$gamma_neartips)) # get the balanced trees
-#' trees2 <- compact(lapply(out, function(x) x$gamma_nearroot)) # get the unbalanced trees
+#' out <- simbal(t = 10, metric = "gamma", n = 100, cutlow = 1.6, cuthigh = 3) # run it
+#' out$bal # get the trees with nodes close to root on average
+#' out$unbal # get the trees with nodes close to the tips on average
 #' @export
-simbal <- function(t = 10, metric = "colless", n = 10, cutlow, cuthigh) 
+simbal <- function(t = 10, metric = "colless", n = 10, 
+									 cutlow = -0.5, cuthigh = 0.5) 
 {
-	trees <- replicate(n, rcoal(t), simplify=F) # make a tree with n species
-
-	beta <- function(x) {
-		t_bal <- NULL
-		t_unbal <- NULL
-		xx <- as.treeshape(x) # convert trees to aptreeshape format trees
-		b <- maxlik.betasplit(xx)[[1]] # calculate beta for all trees
-		if(b < cutlow){ t_unbal <- I(x) } else
-			if(b > cuthigh) { t_bal <- I(x) } # returns tree if less than some level of balance	
-				end
-		compact(list(beta = b, beta_bal = t_bal, beta_unbal = t_unbal))
+	# Function to make trees and calculate beta-splitting metric
+	beta <- function(t = t, n = n, cutlow = cutlow, cuthigh = cuthigh) {
+		iter <- 0
+		trees <- list()
+		while( length(compact(trees)) < n ){
+			iter <- iter + 1
+			tree <- rcoal(t) # make a tree
+			xx <- as.treeshape(tree) # convert to apTreeshape format
+			c_ <- maxlik.betasplit(xx)[[1]] # calculate beta-splitting metric
+			if(c_ < cutlow){ trees[[iter]] <- tree } else { trees[[iter]] <- NULL }
+		}
+		balanced <- compact(trees)
+		
+		iter2 <- 0
+		trees2 <- list()
+		while( length(compact(trees2)) < n ){
+			iter2 <- iter2 + 1
+			tree2 <- rcoal(t) # make a tree
+			xx2 <- as.treeshape(tree2) # convert to apTreeshape format
+			c_ <- maxlik.betasplit(xx2)[[1]] # calculate beta-splitting metric
+			if(c_ > cuthigh){ trees2[[iter2]] <- tree2 } else { trees2[[iter2]] <- NULL }
+		}
+		unbalanced <- compact(trees2)
+		
+		list(bal = balanced, unbal = unbalanced)
 	}
-	colless_ <- function(x) {
-		t_bal <- NULL
-		t_unbal <- NULL
-		xx <- as.treeshape(x) # convert trees to aptreeshape format trees
-		c_ <- colless(xx, "yule") # calculate colless' metric
-		if(c_ < cutlow){ t_bal <- x } else
-			if(c_ > cuthigh) { t_unbal <- x } # returns tree if less than some level of balance	
-				end
-		compact(list(colstat = c_, colless_bal = t_bal, colless_unbal = t_unbal))
+	
+	# Function to make trees and calculate colless' metric
+	colless_ <- function(t = t, n = n, cutlow = cutlow, cuthigh = cuthigh) {
+		iter <- 0
+		trees <- list()
+		while( length(compact(trees)) < n ){
+			iter <- iter + 1
+			tree <- rcoal(t) # make a tree
+			xx <- as.treeshape(tree) # convert to apTreeshape format
+			c_ <- colless(xx, "yule") # calculate colless' metric
+			if(c_ < cutlow){ trees[[iter]] <- tree } else { trees[[iter]] <- NULL }
+		}
+		balanced <- compact(trees)
+		
+		iter2 <- 0
+		trees2 <- list()
+		while( length(compact(trees2)) < n ){
+			iter2 <- iter2 + 1
+			tree2 <- rcoal(t) # make a tree
+			xx2 <- as.treeshape(tree2) # convert to apTreeshape format
+			c_ <- colless(xx2, "yule") # calculate colless' metric
+			if(c_ > cuthigh){ trees2[[iter2]] <- tree2 } else { trees2[[iter2]] <- NULL }
+		}
+		unbalanced <- compact(trees2)
+		
+		list(bal = balanced, unbal = unbalanced)
 	}
-	gammastat <- function(x) {
-		nearroot <- NULL
-		neartips <- NULL
-		c_ <- gammaStat(x) # calculate gamma statistic metric
-		if(c_ < cutlow){ nearroot <- x } else
-			if(c_ > cuthigh) { neartips <- x } # returns tree if less than some level of balance	
-		end
-		compact(list(gamstat = c_, gamma_nearroot = nearroot, gamma_neartips = neartips))
+	
+	# Function to make trees and calculate gammaStat
+	gammastat <- function(t = t, n = n, cutlow = cutlow, cuthigh = cuthigh) {
+		iter <- 0
+		trees <- list()
+		while( length(compact(trees)) < n ){
+			iter <- iter + 1
+			tree <- rcoal(t) # make a tree
+			c_ <- gammaStat(tree) # calculate gamma statistic metric
+			if(c_ > cuthigh){ trees[[iter]] <- tree } else { trees[[iter]] <- NULL }
+		}
+		neartips <- compact(trees)
+		
+		iter2 <- 0
+		trees2 <- list()
+		while( length(compact(trees2)) < n ){
+			iter2 <- iter2 + 1
+			tree2 <- rcoal(t) # make a tree
+			c_ <- gammaStat(tree2) # calculate gamma statistic metric
+			if(c_ < cutlow){ trees2[[iter2]] <- tree2 } else { trees2[[iter2]] <- NULL }
+		}
+		nearroot <- compact(trees2)
+		
+		list(bal = nearroot, unbal = neartips)
 	}
 	
 	metric <- match.arg(metric, choices=c("colless","beta","gamma"), several.ok=F)
 	
 	if(metric == "beta"){
-		betalist <- lapply(trees, beta)
+		betalist <- beta(t=t, n=n, cutlow=cutlow, cuthigh=cuthigh)
 		collesslist <- NULL
 		gammalist <- NULL
 	} else
 		if(metric == "colless"){
 			betalist <- NULL
-			collesslist <- lapply(trees, colless_)
+			collesslist <- colless_(t=t, n=n, cutlow=cutlow, cuthigh=cuthigh)
 			gammalist <- NULL
 		} else
 			if(metric == "gamma"){
 				betalist <- NULL
 				collesslist <- NULL
-				gammalist <- lapply(trees, gammastat)
+				gammalist <- gammastat(t=t, n=n, cutlow=cutlow, cuthigh=cuthigh)
 			} else
 			stop("metric must be one of 'beta', 'colless', or 'gamma'")
 	
