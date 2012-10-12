56abefcea3af9ccafc57fe558a97718b482c3bc8
Scott Chamberlain <myrmecocystus@gmail.com>
2012-10-12 16:16:59 -0700

    added write_fasta for writing fast files

---
+++ R/write_fasta.r
@@ -0,0 +1,24 @@
+#' Write a fasta file to directory to use for whatever, e.g., running Clustal 
+#' 		for multiple sequences alignment. 
+#' 
+#' @param sequences A list of sequences. 
+#' @param fileout A file to write to, e.g., mysequences.fas
+#' @examples 
+#' # Note that they are all the same sequence
+#' myseqs <- c("TCTTATTTACAATAGGAGGATTATCAGGAATTATATTATCAAATTCATCTATTGATATTATACTACACGATACTTATTACGTTATTGGACACTTTCATTATGTACTCTCAATA",
+#' 		"TCTTATTTACAATAGGAGGATTATCAGGAATTATATTATCAAATTCATCTATTGATATTATACTACACGATACTTATTACGTTATTGGACACTTTCATTATGTACTCTCAATA",
+#' 		"TCTTATTTACAATAGGAGGATTATCAGGAATTATATTATCAAATTCATCTATTGATATTATACTACACGATACTTATTACGTTATTGGACACTTTCATTATGTACTCTCAATA")
+#' names(myseqs) <- c("Apis_mellifera","Homo sapiens","Helianthus annuus")
+#' write_fasta(myseqs, "myseqs.fas")
+write_fasta <- function (sequences, fileout) 
+{
+	outfile <- file(description = fileout, open = "w")
+	write.oneseq <- function(sequence, name) {
+		if(grepl("\\s", name)){name<-gsub("\\s", "_", name)} else{name<-name}
+		writeLines(paste(">", name, sep = ""), outfile)
+		writeLines(sequence[[1]], outfile)
+	}
+	sapply(seq_len(length(sequences)), function(x) write.oneseq(
+		sequence=as.character(sequences[[x]]), name=names(sequences[x])))
+	close(outfile)
+}
