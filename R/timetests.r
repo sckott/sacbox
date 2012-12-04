7d3dd8adfa62a0d2deeb8dd98d72819ee9b77a29
Scott Chamberlain <myrmecocystus@gmail.com>
2012-12-04 11:09:14 -0800

    added fxn timetests to simplify benchmarking your test suite

---
+++ R/timetests.r
@@ -0,0 +1,18 @@
+#' Get run times for all tests in a package. 
+#' 
+#' @import reshape plyr
+#' @param dir Path to the directory that contains the tests. 
+#' @return A data.frame sorted by elapsed run time for each test so that you can 
+#' 		easily pick out the offending tests that are taking a long time.
+#' @examples \dontrun{
+#' timetests(dir = "/Users/ScottMac/github/rOpenSci/taxize_/inst/tests")
+#' }
+#' @export 
+timetests <- function(dir) {
+	fnames <- dir(dir)
+	fnamespaths <- paste0(dir, "/", fnames)
+	times <- lapply(fnamespaths, function(x) system.time( test_file(x) ))
+	names(times) <- fnames
+	df <- ldply(times)
+	sort_df(df, "elapsed")	
+}
