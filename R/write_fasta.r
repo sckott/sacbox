282369f4e618bffb9542766a851a02bdc6f5b6cf
schamberlain <myrmecocystus@gmail.com>
2013-02-05 09:12:01 -0800

    added option in write_fasta to write a phylip type fast format, see example output

--- R/write_fasta.r
+++ R/write_fasta.r
@@ -3,24 +3,39 @@
 #' 
 #' @param sequences A list of sequences. 
 #' @param fileout A file to write to, e.g., mysequences.fas
+#' @param format one of Bayesian or maxlik (for maximum likelihood).
 #' @examples \dontrun{
 #' # Note that they are all the same sequence
-#' myseqs <- c("TCTTATTTACAATAGGAGGATTATCAGGAATTATATTATCAAATTCATCTATTGATATTATACTACACGATACTTATTACGTTATTGGACACTTTCATTATGTACTCTCAATA",
-#' 		"TCTTATTTACAATAGGAGGATTATCAGGAATTATATTATCAAATTCATCTATTGATATTATACTACACGATACTTATTACGTTATTGGACACTTTCATTATGTACTCTCAATA",
-#' 		"TCTTATTTACAATAGGAGGATTATCAGGAATTATATTATCAAATTCATCTATTGATATTATACTACACGATACTTATTACGTTATTGGACACTTTCATTATGTACTCTCAATA")
+#' myseqs <- c("TCTTATTTACAATAGGAGGATTATCAGGAATTATATTATCAAATTCATCTATTGATATTATACTACACGATACTTATTACGTTATTGGACACTTTCATTATGTACTCTCAATATCTTATTTACAATAGGAGGATTATCAGGAATTATATTATCAAATTCATCTATTGATATTATACTACACGATACTTATTACGTTATTGGACACTTTCATTATGTACTCTCAATA",
+#' 	"TCTTATTTACAATAGGAGGATTATCAGGAATTATATTATCAAATTCATCTATTGATATTATACTACACGATACTTATTACGTTATTGGACACTTTCATTATGTACTCTCAATATCTTATTTACAATAGGAGGATTATCAGGAATTATATTATCAAATTCATCTATTGATATTATACTACACGATACTTATTACGTTATTGGACACTTTCATTATGTACTCTCAATA",
+#' 	"TCTTATTTACAATAGGAGGATTATCAGGAATTATATTATCAAATTCATCTATTGATATTATACTACACGATACTTATTACGTTATTGGACACTTTCATTATGTACTCTCAATATCTTATTTACAATAGGAGGATTATCAGGAATTATATTATCAAATTCATCTATTGATATTATACTACACGATACTTATTACGTTATTGGACACTTTCATTATGTACTCTCAATA")
 #' names(myseqs) <- c("Apis_mellifera","Homo sapiens","Helianthus annuus")
-#' write_fasta(myseqs, "myseqs.fas")
+#' write_fasta(sequences=myseqs, fileout="myseqs.fas")
 #' }
 #' @export
-write_fasta <- function (sequences, fileout) 
+write_fasta <- function (sequences, fileout, format = 'bayesian') 
 {
 	outfile <- file(description = fileout, open = "w")
-	write.oneseq <- function(sequence, name) {
+	write_oneseq <- function(sequence, name) {
 		if(grepl("\\s", name)){name<-gsub("\\s", "_", name)} else{name<-name}
 		writeLines(paste(">", name, sep = ""), outfile)
 		writeLines(sequence[[1]], outfile)
 	}
-	sapply(seq_len(length(sequences)), function(x) write.oneseq(
-		sequence=as.character(sequences[[x]]), name=names(sequences[x])))
-	close(outfile)
+	write_oneseq_ml <- function(sequence, name) {
+		if(grepl("\\s", name)){name<-gsub("\\s", "_", name)} else{name<-name}
+		writeLines(paste(name, " ", sequence[[1]], sep = ""), outfile)
+	}
+	if(format=="bayesian"){
+		sapply(seq_len(length(sequences)), function(x) write_oneseq(
+			sequence=as.character(sequences[[x]]), name=names(sequences[x])))
+		close(outfile)
+	} else
+	{
+		numtaxa <- length(sequences)
+		numchars <- nchar(sequences[[1]])
+		writeLines(paste(numtaxa, " ", numchars, "\n", sep = ""), outfile)
+		sapply(seq_len(length(sequences)), function(x) write_oneseq_ml(
+			sequence=as.character(sequences[[x]]), name=names(sequences[x])))
+		close(outfile)
+	}
 }
