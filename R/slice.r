cb8f2f35f6a5589c57c02f59b91c4a9b3b6fe30e
schamberlain <myrmecocystus@gmail.com>
2013-02-09 06:28:27 -0800

    added slice function

---
+++ R/slice.r
@@ -0,0 +1,18 @@
+#' Function to break up a vector by certain N sized chunks.
+#' 
+#' @import plyr
+#' @param input Input character vector
+#' @param by Number by which to split the input character vector
+slice <- function(input, by = 2, equalpiecesof = NULL) {
+	if(is.null(equalpiecesof)){	
+		starts <- seq(1, length(input), by)
+		tt <- lapply(starts, function(y) input[y:(y + (by - 1))])
+		llply(tt, function(x) x[!is.na(x)])
+	} else
+	{
+		splitby <- round(length(input)/equalpiecesof)+1
+		starts <- seq(1, length(input), splitby)
+		tt <- lapply(starts, function(y) input[y:(y + (splitby - 1))])
+		llply(tt, function(x) x[!is.na(x)])
+	}
+}
