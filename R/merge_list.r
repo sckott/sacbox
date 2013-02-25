5b4287f76973d5c70f929b7e87b9b5853c43383e
schamberlain <myrmecocystus@gmail.com>
2013-02-25 13:43:49 -0800

    added new fxn merge_list, seems to perform better than merge_recurse and merge_all from reshape

---
+++ R/merge_list.r
@@ -0,0 +1,20 @@
+#' Recursively merge data.frame's
+#' 
+#' @param dfs A list of data.frame's
+#' @param ... Arguments passed onto merge
+#' @author Scott Chamberlain \email{myrmecocystus@@gmail.com}
+#' @examples
+#' mtcars$cars <- row.names(mtcars)
+#' df1 <- mtcars[,c(1:2,12)]
+#' df2 <- mtcars[,c(3:4,12)]
+#' merge_list(list(df1, df2), by="cars")
+#' @export 
+merge_list <- function(dfs, ...)
+{
+	dfs1 <- dfs[[1]]
+	dfs2 <- dfs[-1]
+	for(i in 1:length(dfs2)){
+		dfs1 <- merge(dfs1, dfs2[[i]], all = TRUE, sort = FALSE, ...)
+	}
+	return( dfs1 )
+}
