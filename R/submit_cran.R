4a8abdac5d71c6da0ad206b23612a4225f077d1f
Scott Chamberlain <myrmecocystus@gmail.com>
2020-04-16 08:16:35 -0700

    updates

---
+++ R/submit_cran.R
@@ -0,0 +1,7 @@
+#' @export
+submit_cran <- function(pkg = ".", args = NULL) {
+  pkg <- devtools:::as.package(pkg)
+  built_path <- devtools:::build_cran(pkg, args = args)
+  devtools:::upload_cran(pkg, built_path)
+  usethis::with_project(pkg$path, devtools:::flag_release(pkg))
+}
